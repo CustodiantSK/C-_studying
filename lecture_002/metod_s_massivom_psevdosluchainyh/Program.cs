6fb588ed439f80754f0d01ac57c5e3de13c316a9
CustodiantSK <kripkovsky@yandex.ru>
2023-02-14 18:35:10 +0700

    Создали 2 метода реализующих заполнение массива случайными значениями и вывод на печать массива

---
+++ lecture_002/metod_s_massivom_psevdosluchainyh/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int[] array = new int[10];    //задали массив с длиной в 10 элементов
+
+//метод создания массива заполненого случайными числами
+void FillArray(int[] collection) //void- метод, который ничего не возвращает
+{
+    int lenght = collection.Length; //получение длины массива
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10); // положили в коллекцию случайное число из диапазона 1-10
+        index++;
+    }
+}
+
+// метод печати массива
+void PrintArray(int[] col)
+{
+    int count = col.Length; //получили длину массива
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} "); //прописать значение из индекса
+        position++;
+    }
+}
+
+FillArray(array); //выполнили заполнение массива рандомными значениями
+PrintArray(array);  //вывели отбражение масива 
+
+
+
+
