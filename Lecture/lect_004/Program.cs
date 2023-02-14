9cc2c0c450cc5cebcfeeabeec1ca58bed1e5e2bb
CustodiantSK <kripkovsky@yandex.ru>
2023-02-14 12:45:56 +0700

    удалены старые файлы

--- Lecture/lect_004/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int numA = new Random().Next(1, 11);
-Console.WriteLine(numA);
-int numB = new Random().Next(1, 11);
-Console.WriteLine(numB);
-int result = numA + numB;
-Console.WriteLine(result);
