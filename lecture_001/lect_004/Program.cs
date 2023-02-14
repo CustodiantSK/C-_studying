08884a60e0ed8bfc00bcabe3f32e3a3a1b0043a5
CustodiantSK <kripkovsky@yandex.ru>
2023-02-14 12:13:37 +0700

    Произведено перераспределение папок

---
+++ lecture_001/lect_004/Program.cs
@@ -0,0 +1,6 @@
+﻿int numA = new Random().Next(1, 11);
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 11);
+Console.WriteLine(numB);
+int result = numA + numB;
+Console.WriteLine(result);
---
+++ lecture_001/lect_006/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int a = new Random().Next(0, 1001);
+Console.WriteLine($"A = {a}");
+int b = new Random().Next(0, 1001);
+Console.WriteLine($"B = {b}");
+int c = new Random().Next(0, 1001);
+Console.WriteLine($"C = {c}");
+int d = new Random().Next(0, 1001);
+Console.WriteLine($"D = {d}");
+int e = new Random().Next(0, 1001);
+Console.WriteLine($"E = {e}");
+int f = new Random().Next(0, 1001);
+Console.WriteLine($"F = {f}");
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);
